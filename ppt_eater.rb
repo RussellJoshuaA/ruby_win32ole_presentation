394e1888a966a49878cc1714b9f104d0536f4f03
Josh <russelljoshuaa@gmail.com>
2017-10-02 13:07:55 -0400

    added ppt eater script

---
+++ ppt_eater.rb
@@ -0,0 +1,72 @@
+require 'win32ole'
+
+THIS_DIR = File.dirname(__FILE__)
+
+app = WIN32OLE.new 'PowerPoint.Application'
+presentation = app.Presentations.Open('C:\Users\Joshua Russell\Dropbox\ruby\ruby_win32ole_presentation\OLE Automation (hexagons).pptx')
+
+gets
+
+number_of_slides = presentation.Slides.Count
+slides = []
+number_of_slides.times do |index|
+  slides << presentation.Slides(index + 1)
+end
+
+slides.reverse.each do |slide|
+  sleep 0.2
+  slide.select
+
+  shapes = []
+  number_of_shapes = slide.Shapes.Count
+
+  if number_of_shapes == 0
+    slide.Delete
+    next
+  end
+
+  number_of_shapes.times do |index|
+    shapes << slide.Shapes(index + 1)
+  end
+
+  direction = [:west, :east].sample
+
+  case direction
+    when :west
+      pacman_origin_x = 570
+      pacman_origin_y = 25 + Random.rand(400)
+      pacman_movement_increment = 600 / number_of_shapes
+      pacman = slide.Shapes.AddPicture('c:\Users\Joshua Russell\Dropbox\ruby\ruby_win32ole_presentation\pacman.png', true, true, pacman_origin_x, pacman_origin_y)
+    when :east
+      pacman_origin_x = 25
+      pacman_origin_y = 25 + Random.rand(400)
+      pacman_movement_increment = 0 - (600 / number_of_shapes)
+      pacman = slide.Shapes.AddPicture('c:\users\Joshua Russell\Dropbox\ruby\ruby_win32ole_presentation\pacman.png', true, true, pacman_origin_x, pacman_origin_y)
+      pacman.Flip(0)
+  end
+
+  shapes.shuffle.each do |shape|
+    sleep 0.1
+    pacman.Left = pacman.Left - pacman_movement_increment
+    shape.Delete
+  end
+
+  slide.Delete
+end
+
+new_slide = presentation.Slides.Add(1, 1)
+sleep 0.5
+new_slide.Shapes(1).TextFrame.TextRange.Text = 'Thanks for Listening!'
+sleep 0.5
+loop do
+  subtitles = [
+      'Joshua Russell', 'Joshua Russell .', 'Joshua Russell ..', 'Joshua Russell ...',
+      'russelljoshuaa@gmail.com', 'russelljoshuaa@gmail.com .', 'russelljoshuaa@gmail.com ..', 'russelljoshuaa@gmail.com ...',
+      '@russelljoshuaa', '@russelljoshuaa .', '@russelljoshuaa ..', '@russelljoshuaa ...',
+      'https://www.linkedin.com/in/joshua-russell-ab139660/', 'https://www.linkedin.com/in/joshua-russell-ab139660/ .', 'https://www.linkedin.com/in/joshua-russell-ab139660/ ..', 'https://www.linkedin.com/in/joshua-russell-ab139660/ ...'
+  ]
+  subtitles.each do |subtitle|
+    new_slide.Shapes(2).TextFrame.TextRange.Text = subtitle
+    sleep 0.4
+  end
+end
